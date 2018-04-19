5050c791daadd242a49dcec486a1a31511c42107
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-04-19 23:59:45 +0530

    DONE

---
+++ using-spring-boot/src/main/java/narayan/swagger/integration/kit/SwaggerContext.java
@@ -0,0 +1,16 @@
+package narayan.swagger.integration.kit;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 
+ * @author narayan-sambireddy
+ *
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerContext {
+
+}
